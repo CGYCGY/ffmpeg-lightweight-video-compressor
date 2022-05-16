d0ea1fcee6a329577d4ca03e04c0bc5a2047343c
Cheong Gwen Yit <cgyyit@gmail.com>
2022-05-16 23:33:10 +0800

    converter main code

---
+++ converter.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+
+def check_exist(hevc, o):
+    # check converted filename existence
+    if os.path.exists(hevc):
+        print(hevc)
+        if o == '':
+            while True:
+                ov = input('File exist, overwrite file? (y/n)')
+                if ov.lower() == 'n':
+                    return False
+                elif ov.lower() == 'y':
+                    return '-y'
+                else:
+                    print('Invalid input')
+        elif not o:
+            print('File exist, skipped')
+            return False
+    return o
+
+
+def convert_to_mp4(bd, r, fn, o):
+    ori = os.path.join(bd, r[2:], fn)
+    if not ori.lower().endswith('.mp4'):
+        mp4 = os.path.splitext(ori)[0] + '.mp4'
+        o = check_exist(mp4, o)
+        if o:
+            print('convert', ori, 'to', mp4, sep='\n')
+            os.system('ffmpeg {overwrite} -i "{source}" -c copy "{output}"'.format(source=ori, output=mp4, overwrite=o))
+
+
+def compress(bd, r, fn, o):
+    ori = os.path.join(bd, r[2:], fn)
+    codec = os.popen('ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of '
+                     'default=noprint_wrappers=1:nokey=1 "{0}"'.format(ori)).read()
+    # check codec
+    if codec.strip() != 'hevc':
+        pos = len(ori) - 4
+        temp = ori[:pos] + '_' + ori[pos:]
+        hevc = os.path.splitext(ori)[0] + '.mp4'
+
+        o = check_exist(hevc, o)
+        if o:
+            print('Start compress', ori, sep='\n')
+            os.rename(ori, temp)
+            os.system('ffmpeg {overwrite} -i "{source}" -vcodec libx265 -crf 30 "{output}"'
+                      .format(source=temp, output=hevc, overwrite=o))
+            os.remove(temp)
+            print('Compressed')
+
+
+def main(basedir=os.getcwd(), overwrite=''):
+    print('Selected Folder', '===============', basedir, '===============', sep='\n')
+    # loop files
+    for root, dirs, files in os.walk(basedir):
+        for filename in files:
+            if filename.lower().endswith('.ts') or filename.lower().endswith('.mp4'):
+                compress(basedir, root, filename, overwrite)
+
+    print('Completed')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main()
---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+from converter import main
+
+
+options = ['', '-y', False, '-n']
+
+
+def load_folder():
+    file_path_label['text'] = filedialog.askdirectory()
+    if not file_path_label['text']:
+        file_path_label['text'] = os.getcwd()
+
+
+def start_compress():
+    global options
+    if file_path_label['text'] and file_path_label['text'] != 'Select A Folder':
+        # os.system('python video_converter.py ' + file_path_label['text'])
+        main(file_path_label['text'], options[option.get()])
+    else:
+        main(overwrite=options[option.get()])
+
+
+root = tk.Tk()
+root.title('Video Compressor')
+root.geometry("300x350")
+
+option = tk.IntVar(value=0)
+
+load_button = tk.Button(root, command=load_folder, text='Load', bg='grey', fg='white')
+file_path_label = tk.Label(root, text=os.getcwd(), fg='green', font=('helvetica', 12, 'bold'), wraplength=190)
+option_button_1 = tk.Radiobutton(root, text='Ask everytime', variable=option, value=0)
+option_button_2 = tk.Radiobutton(root, text='Overwrite files if exist', variable=option, value=1)
+option_button_3 = tk.Radiobutton(root, text='Skip files if exist', variable=option, value=2)
+option_button_4 = tk.Radiobutton(root, text='Delete files if exist', variable=option, value=3)
+compress_button = tk.Button(root, command=start_compress, text='Compress!', bg='brown', fg='white')
+
+load_button.pack(pady=10)
+file_path_label.pack()
+option_button_1.pack()
+option_button_2.pack()
+option_button_3.pack()
+option_button_4.pack()
+compress_button.pack(pady=10)
+
+root.mainloop()
